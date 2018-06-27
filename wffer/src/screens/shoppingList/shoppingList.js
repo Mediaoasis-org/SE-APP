1cdc9c132f1e336f0ec87b115fa925e6077f5428
rashmi_bd <rashmi_bd@bdappmaniac.com>
2018-06-27 18:17:06 +0530

    my wishlist show is done

--- wffer/src/screens/shoppingList/shoppingList.js
+++ wffer/src/screens/shoppingList/shoppingList.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   Image,
   Platform,
-  
+  AsyncStorage,
   FlatList,
   ScrollView,
   StyleSheet
@@ -26,7 +26,6 @@ const SECTIONS = [
   {
     title: 'My Shopping List',
     listname:'qwerty',
-
   },
   {
     title: 'Recommended List',
@@ -39,13 +38,27 @@ export class ShoppingListComponent extends Component {
 	constructor(props){
 		super(props);
 		this.state={
+      oauthToken:'',
+      oauthSecret:'',
+      mysection:false,
+      recommendedsection:false,
       activeSection: false,
       collapsed: true,
        search :'',
+       fieldValues:[],
+       totalItems:''
 		}
+    this.getStorageValues()
+    
 		// alert(JSON.stringify(this.props.navigation))
 	}
-	  
+	async getStorageValues(){
+         var userData = await AsyncStorage.getItem('userData');
+         this.setState({userData:JSON.parse(userData)});
+         this.setState({oauthToken:this.state.userData.oauth_token});
+         this.setState({oauthSecret:this.state.userData.oauth_secret});
+         this.fetchValues();
+   }
 
 	 // _renderSectionTitle(section) {
   //   return (
@@ -54,55 +67,116 @@ export class ShoppingListComponent extends Component {
   //     </View>
   //   );
   // }
+  fetchValues(){
+      return fetch('https://wffer.com/se/api/rest/listings/wishlist?oauth_consumer_key=mji82teif5e8aoloye09fqrq3sjpajkk&oauth_consumer_secret=aoxhigoa336wt5n26zid8k976v9pwipe&oauth_token='+ this.state.oauthToken + '&oauth_secret=' +this.state.oauthSecret + '&search_wishlist=my_wishlists',{
+              method:'GET'
+            })
+            .then((response) => response.json())
+            .then((responseJson) => {
+              if(responseJson.status_code=='200'){
+                // alert(JSON.stringify(responseJson.body));
+                 this.setState({
+                  fieldValues:responseJson.body.response,
+                  totalItems: responseJson.body.totalItemCount,
+                });
+              }
+              else
+              {
+                // 
+              }
+              this.setState({Message:responseJson.Message});
+            })
+            .catch((error) =>{
+              console.error(error);
+            });
+  }
   handleNavigation(){
     this.props.navigation.navigate('Wishlists');
   }
-  _renderHeader(section) {
-    return (
-      <View style={styles.header}>
-        <Text style={styles.headerText}><Icon name="angle-down" size={24} color="#000" style={{flexDirection:'column'}} />  {section.title}</Text>
-      </View>
-    );
+  // _renderHeader(section) {
+  //   return (
+  //     <View style={styles.header}>
+  //       <Text style={styles.headerText}><Icon name="angle-down" size={24} color="#000" style={{flexDirection:'column'}} />  {section.title}</Text>
+  //     </View>
+  //   );
+  // }
+  renderMyList(){
+    if(this.state.fieldValues.length===0){
+       return(
+            <View style={styles.content}>
+              <Text style={styles.shoppingText}>No data found</Text>
+            </View>);
+    }
+    else
+    {
+    return(
+          this.state.fieldValues.map((item)=>{
+            // alert(item.listing_images_1['image'])
+            let img = 'uri:listing_images_'+item.total_item;
+            // console.log(img);
+            // console.log('item.'+img);
+            return(
+              <View style={styles.content} key={item.wishlist_id}>
+                  <TouchableOpacity onPress={()=>this.handleNavigation()} >
+                    <View style={{borderColor:'#000',borderWidth:1,padding:10,marginTop:8,flexDirection:'row'}}>
+                        <View style={{flexDirection:'column',width:'20%'}}>
+                        
+                            <Image source={{uri:img}} resizeMode="contain" style={{width:'100%'}}/>
+                        </View>
+                        <View style={{flexDirection:'column',width:'80%'}}>
+                            <Text style={{fontSize:16,fontWeight:'bold',marginTop:5,color:'#000'}}>{item.title}</Text>
+                            <Text style={{marginTop:5,color:'#000'}}>{item.total_item} entries</Text>
+                            <Text style={{marginTop:5,color:'#000'}}>{item.creation_date}</Text>
+                            <Text style={{marginTop:5,color:'#000'}}>{item.body}</Text>
+                        </View>
+                    </View>
+                  </TouchableOpacity>
+              </View>
+              );
+          })
+        );
+      }
   }
 
-  _renderContent = (section) => {
+  // _renderContent = (section) => {
      
-        if(section.listname!=""){
-            return (
-            <View style={styles.content}>
-                <TouchableOpacity onPress={()=>this.handleNavigation()} >
-                  <Text style={styles.shoppingText}>shopping list found</Text>
-                  <View style={{borderColor:'#000',borderWidth:1,padding:10,marginTop:8,flexDirection:'row'}}>
-                      <View style={{flexDirection:'column',width:'20%'}}>
-                          <Image source={require('../../../assets/nophoto.png')} resizeMode="contain" style={{width:'100%'}}/>
-                      </View>
-                      <View style={{flexDirection:'column',width:'80%'}}>
-                          <Text style={{fontSize:16,fontWeight:'bold',marginTop:5,color:'#000'}}>{section.listname}</Text>
-                          <Text style={{marginTop:5,color:'#000'}}>0 entries</Text>
-                          <Text style={{marginTop:5,color:'#000'}}>Wed at 5:18 PM</Text>
-                          <Text style={{marginTop:5,color:'#000'}}>list note</Text>
-                      </View>
-                  </View>
-                </TouchableOpacity>
-            </View>
-            );
-        }
-        else
-        {
-            return(
-                <View style={styles.content}>
-                  <Text style={styles.shoppingText}>No data found</Text>
-                </View>
-            );
+  //       if(section.listname!=""){
+  //           return (
+  //           <View style={styles.content}>
+  //               <TouchableOpacity onPress={()=>this.handleNavigation()} >
+  //                 <Text style={styles.shoppingText}>shopping list found</Text>
+  //                 <View style={{borderColor:'#000',borderWidth:1,padding:10,marginTop:8,flexDirection:'row'}}>
+  //                     <View style={{flexDirection:'column',width:'20%'}}>
+  //                         <Image source={require('../../../assets/nophoto.png')} resizeMode="contain" style={{width:'100%'}}/>
+  //                     </View>
+  //                     <View style={{flexDirection:'column',width:'80%'}}>
+  //                         <Text style={{fontSize:16,fontWeight:'bold',marginTop:5,color:'#000'}}>{section.listname}</Text>
+  //                         <Text style={{marginTop:5,color:'#000'}}>0 entries</Text>
+  //                         <Text style={{marginTop:5,color:'#000'}}>Wed at 5:18 PM</Text>
+  //                         <Text style={{marginTop:5,color:'#000'}}>list note</Text>
+  //                     </View>
+  //                 </View>
+  //               </TouchableOpacity>
+  //           </View>
+  //           );
+  //       }
+  //       else
+  //       {
+  //           return(
+  //               <View style={styles.content}>
+  //                 <Text style={styles.shoppingText}>No data found</Text>
+  //               </View>
+  //           );
   
-        }
+  //       }
       
       
-  }
+  // }
 
  
 
 	render(){
+    
 		return(
 			<View style={gstyles.container}>
 					<View style={gstyles.headerMenu}>
@@ -113,11 +187,27 @@ export class ShoppingListComponent extends Component {
                 <TouchableOpacity onPress={()=>this.props.navigation.navigate('CreateWishlist')} style={gstyles.headerRightButton}><Icon name="plus-circle" size={24} color="#fff" /></TouchableOpacity>
 					</View>
           <SearchComponent />
-					<Accordion
-        	  sections={SECTIONS}
-					  renderHeader={this._renderHeader}
-					  renderContent={this._renderContent}
-					/>
+				  <ScrollView>
+          <View>
+           <View style={styles.header}>
+            <TouchableOpacity onPress={()=>this.setState({mysection:!this.state.mysection,recommendedsection:false})}><Text style={styles.headerText}><Icon name="angle-down" size={24} color="#000" style={{flexDirection:'column'}} />  My Shopping List</Text></TouchableOpacity>
+             
+            </View>
+            {
+              this.state.mysection==true ? <Text style={[styles.shoppingText],{padding:10}}>{this.state.totalItems} shopping lists found</Text> : null
+            }
+            {
+              this.state.mysection==true ? this.renderMyList() : null
+            }
+          </View>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={()=>this.setState({recommendedsection:!this.state.recommendedsection,mysection:false})}><Text style={styles.headerText}><Icon name="angle-down" size={24} color="#000" style={{flexDirection:'column'}} />  Recommended List</Text></TouchableOpacity>
+
+            </View>
+             {
+              this.state.recommendedsection==true ? <View style={styles.content}><Text style={styles.shoppingText}>No data found</Text></View> : null
+            }
+          </ScrollView>
 			</View>
 		);
 	}
@@ -150,7 +240,8 @@ const styles = StyleSheet.create({
     fontSize:20
   },
   content: {
-    padding: 20,
+    paddingLeft: 10,
+    paddingRight:10,
     backgroundColor: '#fff',
 
   },
@@ -178,3 +269,9 @@ const styles = StyleSheet.create({
     padding: 10
   }
 });
+// 
+  // <Accordion
+  //           sections={SECTIONS}
+  //           renderHeader={this._renderHeader}
+  //           renderContent={this._renderContent}
+  //         />
